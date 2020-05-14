4a8f7bc3cd0bd05db8c3ff8e80c034ca696fa558
adam <azherskw@gmail.com>
2020-05-13 23:26:22 -0400

    Use ws instead of xmlhttprequest

--- script.js
+++ script.js
@@ -1,124 +1,114 @@
 window.onload = function() {
-
-    let rr = 1.5; // refresh rate in seconds
-    let mode = 'btc';
-
-    let price = new Vue({
-        el: '#container',
-        data: {
-            currentPrice: 0,
-            animatedPrice: 0,
-            currency: 'Bitcoin'
-        },
-        watch: {
-            currentPrice: function(newValue, oldValue) {
-                var vm = this;
-				debugValues(newValue, oldValue);
-				let newHtml = renderUpdatedPrice(newValue, oldValue);
-				renderDiff(newValue, oldValue);
-				if (newHtml.length > 0) {
-					document.getElementById("animatedPrice").innerHTML = newHtml;
-				}
-            }
+  let rr = 3; // refresh rate in seconds
+  let price = new Vue({
+    el: '#container',
+      data: {
+        currentPrice: 0,
+        animatedPrice: 0,
+      },
+      watch: {
+        currentPrice: function(newValue, oldValue) {
+          renderUpdatedPrice(newValue, oldValue);
+          renderDiff(newValue, oldValue);
         }
-    });
+      }
+  });
 
-    //price.currentPrice = 15000;
-    getCurrentPrice(mode, price);
-    setInterval(function() {
-        //price.currentPrice += 100;
-        getCurrentPrice(mode, price);
-    }, rr * 1000);
+  getCurrentPrice(price);
+  setInterval(function() {
+    getCurrentPrice(price);
+  }, rr * 1000);
+}
+
+const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
+ws.onmessage = function(evt) {
+  try {
+    let msg = JSON.parse(evt.data);
+    $("#lastPrice").val(msg.p);
+  }
+  catch(e) {
+    console.log(evt.data, e);
+  }
 }
 
 function renderUpdatedPrice(newValue, oldValue) {
   let diff = (newValue - oldValue).toFixed(2);
-  if (diff === 0 || oldValue <= 0 || newValue <= 0) {
-	return "";
+  if (parseFloat(diff) === 0 || oldValue <= 0 || newValue <= 0) {
+    return;
   }
   let strNewValue = (parseFloat(newValue).toFixed(2)).toString();
   let strOldValue = (parseFloat(oldValue).toFixed(2)).toString();
   if (strNewValue === strOldValue) {
-	return "";
+    return;
   }
   let html = "";
-  let isUp = newValue >= oldValue;
-  let firstDiffIndex = getFirstDiffIndex(strNewValue, strOldValue);
-  if (strNewValue.length != strOldValue.length){
-	firstDiffIndex = 0;
+  let isUp = parseFloat(diff) > 0;
+  let digitsLengthChanged = false;
+  if (strNewValue.length !== strOldValue.length){
+    digitsLengthChanged = true;
+    if (strNewValue.length > strOldValue.length) {
+      strOldValue = "0" + strOldValue;
+    }
+    else {
+      strNewValue = "0" + strNewValue;
+    }
   }
+  let firstDiffIndex = getFirstDiffIndex(strNewValue, strOldValue), i;
   for (i = 0; i < strNewValue.length; i++){
-    if (i < firstDiffIndex || strNewValue.charAt(i) === ".") {
-		html = html + "<span>" + strNewValue.charAt(i) + "</span>";
-	}
-	else if (isUp) {
-		html = html + "<span class='pump'>" + strNewValue.charAt(i) + "</span>";
-	}
-	else {
-		html = html + "<span class='dump'>" + strNewValue.charAt(i) + "</span>";
-	}
+    if (i === 0 && digitsLengthChanged) {
+      if (isUp) {
+        html = html + "<span class='pump' id='tp0' data-link='" + strNewValue.charAt(0) +"'>&#8205;</span>";
+      }
+      else {
+        html = html + "<span class='dump' id='tp0' data-link='&#8205;'>" + strOldValue.charAt(0) + "</span>";
+      }
+    }
+    else if (i < firstDiffIndex || strNewValue.charAt(i) === "." || strOldValue.charAt(i) === strNewValue.charAt(i)) {
+      html = html + "<span id='tp" + i + "'>" + strNewValue.charAt(i) + "</span>";
+    }
+    else if (isUp) {
+      html = html + "<span class='pump' id='tp" + i + "' data-link='" + strNewValue.charAt(i) +"'>" + strOldValue.charAt(i) + "</span>";
+    }
+    else {
+      html = html + "<span class='dump' id='tp" + i + "' data-link='" + strNewValue.charAt(i) +"'>" + strOldValue.charAt(i) + "</span>";
+    }
   }
-  return html;
+  document.getElementById("animatedPrice").innerHTML = html;
+  let strClass = isUp ? "pump" : "dump";
+  $("." + strClass).each(function(){
+    let newValue = $(this).attr("data-link");
+    $(this).removeClass(strClass);
+    $(this).fadeOut(function() {
+      $(this).addClass(strClass);
+      $(this).text(newValue).fadeIn();
+    });
+  });
 }
 
 function renderDiff(newValue, oldValue) {
   let diff = (newValue - oldValue).toFixed(2);
-  if (diff === 0 || oldValue <= 0 || newValue <= 0) {
-	return;
+  if (parseFloat(diff) === 0 || oldValue <= 0 || newValue <= 0) {
+    return;
   }
   document.getElementById("diffUp").innerHTML = "&#8205;";
   document.getElementById("diffDown").innerHTML = "&#8205;";
-  let isUp = newValue >= oldValue;
+  let isUp = parseFloat(diff) > 0;
   if (isUp) {
-	document.getElementById("diffUp").innerHTML = "+" + diff;
+    document.getElementById("diffUp").innerHTML = "+" + diff;
   }
   else {
-	document.getElementById("diffDown").innerHTML = diff;  
+    document.getElementById("diffDown").innerHTML = diff;
   }
-}
-
-function debugValues(newValue, oldValue) {
-  let diff = (newValue - oldValue).toFixed(2);
-  if (diff === 0 || oldValue <= 0 || newValue <= 0) {
-    document.getElementById("debugDiff").innerHTML = "";
-	return;
-  }
-  document.getElementById("debugDiff").innerHTML = (newValue - oldValue).toFixed(2);
-  let strNewValue = (parseFloat(newValue).toFixed(2)).toString();
-  let strOldValue = (parseFloat(oldValue).toFixed(2)).toString();
-  if (strNewValue === strOldValue) {
-    document.getElementById("debugFirstChangedIndex").innerHTML = "";
-	return;
-  }
-  if (strNewValue.length != strOldValue.length){
-	document.getElementById("debugFirstChangedIndex").innerHTML = 0;
-	// todo: shift of number of dollar digits, i.e.: 9999 to 10000 or 1000 to 999
-	return;
-  }
-  let firstDiffIndex = getFirstDiffIndex(strNewValue, strOldValue);
-  document.getElementById("debugFirstChangedIndex").innerHTML = firstDiffIndex;
 }
 
 function getFirstDiffIndex(n, o) {
-  var i = 0;
+  let i = 0;
   if (n === o) return -1;
   while (n[i] === o[i]) i++;
   return i;
 }
 
-function getCurrentPrice(coin, vue) {
-    let http = new XMLHttpRequest();
-    let cors_proxy = 'https://cors-anywhere.herokuapp.com/';
-    if (coin == 'btc') {
-		var api = 'https://www.bitstamp.net/api/ticker/';
-    } 
-    http.open("GET", api, true);
-    http.onload = function() {
-        var data = JSON.parse(http.responseText);
-		vue.currentPrice = data.last;
-    }
-    http.send();
+function getCurrentPrice(vue) {
+  vue.currentPrice = $("#lastPrice").val();
+  // vue.currentPrice = (Math.random() * (10010.99 - 9990.01) + 9990.01).toFixed(2);  // test numbers
 }
-
-
-
